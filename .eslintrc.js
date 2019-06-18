1ab746681553e92da8e3eb824a4b9ac3abe140dc
liuyifan <3503238271@qq.com>
2019-06-18 17:18:06 +0800

    chapters2

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,8 +22,11 @@ module.exports = {
   // add your custom rules here
   rules: {
     // allow async-await
-    'generator-star-spacing': 'off',
+    'arrow-parens':0,
+    'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'eol-last':0,
+    'space-before-function-paren':0
   }
 }
